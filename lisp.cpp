8df8f9d179000303b0e9acac731a61b368ca8652
Thomas <thomasschwalen@gmail.com>
2019-12-16 01:37:27 -0600

    Simple, almost correct, nested S-exp calculator

--- lisp.cpp
+++ lisp.cpp
@@ -2,7 +2,8 @@
 #include <ctype.h>
 #include <string>
 #include <vector> 
-#include <iostream> 
+#include <iostream>
+#include <unordered_map>  
 
 
 using std::string;
@@ -27,7 +28,7 @@ std::vector<std::string> tokenize( std::string &chars ) {
 
 	std::vector<std::string> tokens;
 
-	std::cout << chars << "\n";
+	//std::cout << chars << "\n";
 
 	int start = 0;
 	int end = 0;
@@ -65,15 +66,90 @@ std::vector<std::string> tokenize( std::string &chars ) {
 		}
 	}
 
+	/*
 	printf("\n");
 	for(std::vector<std::string>::const_iterator i = tokens.begin(); i != tokens.end(); ++i) {
 	    // process i
 	    std::cout << *i << "\n"; // this will print all the contents of *features*
-	}	 
+	}
+	*/
+
 
 	return tokens;
 }
 
+int apply_op(std::string &op, int a, int b) {
+
+	if(op == "+") {
+		return a + b;
+	}
+	else if( op == "-") {
+		return a - b;
+	}
+	else if(op == "*") {
+		return a * b;
+	}
+	return a;
+
+}
+
+
+// let's just try handling some simple operations with any number of expressions first
+int eval( std::vector<string> &expression, int start=0 ) {
+
+	/*
+	   |
+	   v
+	[ '(', +, 3, 4, 5 ')' ]
+
+	       |
+	       v
+	[ '(', +, 3, 4, 5 ')' ]
+
+			  |
+	          v
+	[ '(', +, 3, 4, 5 ')' ]
+
+	*/
+
+	int index = start + 1;
+	
+
+	std::string op = expression[index];
+	index++;
+
+
+	// TODO: make it possible for this to be a sub S-exp too
+	int left = std::stoi(expression[index]);
+	int right = 0;
+
+
+	while (index < expression.size()) {
+
+		if( expression[index + 1] == ")" )
+			break;
+		else if( expression[index + 1] == "(" ) {
+			right = eval(expression, index + 1);
+
+			// hacky but it works
+			while(expression[index] != ")")
+				++index;
+			--index;
+		}
+		else {
+			right = std::stoi(expression[index + 1]);
+		}
+
+		
+		left = apply_op(op, left, right);
+
+		++index;
+	}
+
+
+	//std::cout << left << "_\n";
+	return left;
+}
 
 
 
@@ -82,5 +158,9 @@ int main( int argc, const char* argv[] )
 {
 
 	std::string temp = argv[1];
-	tokenize(temp);
+	std::vector<string> expr = tokenize(temp);
+
+	int r = eval(expr);
+
+	std::cout << r << "\n";
 }
